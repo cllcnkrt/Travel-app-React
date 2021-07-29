6ad2bed377b7d7bb150dbe178f7d61e75025091d
cllcnkrt <cllcnkrt@outlook.com>
2021-07-29 23:14:20 +0300

    footer component

---
+++ src/components/pages/Services.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../App.css'
+import background from 'public/images/img-2.jpg';
+
+function Products() {
+    return (
+       <>
+       <h1 className="services" >SERVİCES</h1>
+       </>
+            
+    )
+    
+}
+
+export default Products
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../App.css'
+
+
+function Products() {
+    return (
+       <>
+       <h1 className="sign-up" >SIGN UP</h1>
+       </>
+    )
+        
+    
+}
+
+export default Products
